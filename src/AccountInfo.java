12804e29437fb43e1c20dea5acc29df84c10811c
Leonardo Abraao <Leonardo.fznd@gmail.com>
2023-10-13 23:30:05 -0300

    adicionado alguns comentarios em AccountInfo.java

--- src/AccountInfo.java
+++ src/AccountInfo.java
@@ -1,19 +1,25 @@
 import java.util.Random;
 
 public class AccountInfo {
+    //Agency number.
     String agency = "077" ;
+
+    //Generate a random number to a account.
     int accountGenerator(int max, int min){
         Random random = new Random();
         return (random.nextInt(max - (min - 1)) + min);
     }
     int numberAccount = accountGenerator(99999999, 9999999);
     int checkerNumber = new Random().nextInt(9);
+
+    //Generate a random number for funds.
     double accountFunds = numberGenerator(9999, 999);
     double numberGenerator(double max, double min){
         Random random = new Random();
         return (random.nextDouble(max - (min - 1)) + min);
     }
 
+    //String for storage the account titular name.
     String titularAccount;
     String typeOfAccount = "Corrente";
 
